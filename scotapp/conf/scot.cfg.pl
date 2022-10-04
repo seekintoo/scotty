6d0f496bc2f0c69c5e7d1008092f2670033f5b9f
Jay Heidecker <jheidecker@seekintoo.com>
2022-10-03 19:00:19 -0600

    finally working local and in container with submod

--- scotapp/conf/scot.cfg.pl
+++ scotapp/conf/scot.cfg.pl
@@ -75,7 +75,7 @@
         logger_name     => 'SCOT',
         layout          => '%d %7p [%P] %15F{1}: %4L %m%n',
         appender_name   => 'scot_log',
-        logfile         => '/home/scot/logs/scot.log',
+        logfile         => './logs/scot.log',
         log_level       => 'DEBUG',
     },
 
---
+++ scotapp/localsetup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH="$HOME/local/bin:${PATH}"
+export PERL5LIB="`pwd`/scot/lib:$HOME/local/lib/perl5"
+mkdir logs
