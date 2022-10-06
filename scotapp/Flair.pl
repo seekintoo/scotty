5d7797dc3591416ac5a10a79f29e3edc6dba4db5
Jay Heidecker <jheidecker@seekintoo.com>
2022-10-06 15:30:30 -0600

    better flair

--- scotapp/Flair.pl
+++ scotapp/Flair.pl
@@ -6,31 +6,30 @@ use lib '../lib';
 use lib '../../lib';
 use lib '../../Scot-Internal-Modules/lib';
 use lib '/opt/scot/lib';
-use v5.16;
-use Scot::App::Responder::Flair;
-use Scot::Collection::Entitytype;
+# use Scot::Flair::Worker;
+use Scot::Flair::Worker;
+use Scot::Env;
 use Data::Dumper;
+use utf8::all;
+use Carp qw(cluck longmess shortmess);
+use feature qw(say);
 
-my $config_file = $ENV{'scot_app_flair_config_file'} //
-                        './conf/flair.cfg.pl';
-my $env = Scot::Env->new(
-    config_file => $config_file,
-);
+my $env     = Scot::Env->new(config_file => './conf/flair.cfg.pl');
+my $log     = $env->log;
 
-$SIG{__DIE__} = sub { our @reason = @_ };
+die unless defined $env and ref($env) eq "Scot::Env";
+
+$SIG{__DIE__} = sub { our @reason =@_};
 
 END {
     our @reason;
     if (@reason) {
-        say "Flairer died because: @reason";
-        $env->log->error("Flairer died because: ",{filter=>\&Dumper, value=>\@reason});
+        say "Flair Diead because: @reason";
+        $env->log->error("Flair died because: ", {filter => \&Dumper, value =>\@reason});
     }
 }
 
 
-
-
-my $loop    = Scot::App::Responder::Flair->new({ 
-    env => $env,
-});
+my $loop    = Scot::Flair::Worker->new(env => $env);
 $loop->run();
+
